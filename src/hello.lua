3ff57d55d05f0885bd7782496bd2df46d515c2ca
xiedakai <xiedakai@treefinance.com.cn>
2019-09-05 15:11:37 +0800

    为了研究redisson中lua脚本中的struct.pack,struct.unpack,拉了struct的源代码,测试了下这个类的用法

---
+++ src/hello.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/env lua
+package.path = "/Users/dashu1/devsoft/luarocks-install-dir/share/lua/5.1/?.lua;"
+           .."/Users/dashu1/devsoft/luarocks-install-dir/share/lua/5.1/md5;"
+           .."/Users/dashu1/devsoft/luarocks-install-dir/share/lua/5.1/?.lua;"
+        ..package.path
+
+print("package.path= "..package.path)
+
+-- require('md5')
+-- local struct = require"struct"
+
+local a=100
+print('hello')
+
+print('world')
+
+print('end')
